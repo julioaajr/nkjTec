5f81117d109d2ba4b269b2205cd3fd3f4896b2d4
julioaajr <61034583+julioaajr@users.noreply.github.com>
2022-01-28 17:58:36 -0300

    preparacao heroku

--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -139,3 +140,4 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
+django_heroku.settings(locals())
