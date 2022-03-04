451a959eb0c1e4832cbe41ade5af688695988835
julioaajr <61034583+julioaajr@users.noreply.github.com>
2022-03-04 16:36:57 -0300

    teste

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn project.wsgi --log-file -
+web: gunicorn --chdir project.wsgi --log-file -
--- users/models.py
+++ users/models.py
@@ -5,10 +5,8 @@ from django.db.models.functions.text import Upper
 # Create your models here.
 
 class User(AbstractUser):
-    """
     first_name = models.CharField(max_length=255, blank=True,null=True, verbose_name="Nome Completo")
     username = models.CharField(max_length=150, unique=True, verbose_name= "CPF / CNPJ (login)")
-    """
     tel = models.CharField(max_length=11, blank=True,null=True, verbose_name="Telefone")
     professional = models.BooleanField(default=False)
     master = models.ForeignKey('User', blank=True,null=True, on_delete=models.PROTECT)
